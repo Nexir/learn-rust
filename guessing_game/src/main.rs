973708771e3a26b404febe7dcbbec5500623a614
NeXiR <nexir49@gmail.com>
2025-05-29 20:19:48 +0200

    add lose codition to guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,12 +1,15 @@
 use rand::Rng;
 use std::{cmp::Ordering, io};
 
+const MAX_TRIES: u8 = 10;
+
 fn main() {
     println!("Guess the number!");
 
     // Variable immutable by default
     // Type inference
     let secret_number = rand::thread_rng().gen_range(1..100); // 1..100 range from 1 inclusive to 100 exclusive
+    let mut nb_guesses = 0;
 
     loop {
         println!("Please input your guess.");
@@ -29,6 +32,8 @@ fn main() {
             }
         };
 
+        nb_guesses += 1;
+
         println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
@@ -36,8 +41,16 @@ fn main() {
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
                 println!("You win!");
+                println!("You found the secret number in {nb_guesses} guesses.");
                 break;
             }
+        }
+
+        if nb_guesses >= MAX_TRIES {
+            println!("You lose!");
+            break;
+        } else {
+            println!("You have {} guesses left.", MAX_TRIES - nb_guesses)
         }
     }
 }
